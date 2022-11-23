f2066194576fccad439296e62524925a90241c46
oguzhan asikkaya <oguzhanasikkaya1@gmail.com>
2022-11-23 23:52:26 +0300

    step definition class fixed

--- src/test/java/com/cydeo/pages/Oguz_US1531_AddingMessagePage.java
+++ src/test/java/com/cydeo/pages/Oguz_US1531_AddingMessagePage.java
@@ -30,8 +30,11 @@ public class Oguz_US1531_AddingMessagePage {
     @FindBy(id = "linkidPostFormLHE_blogPostForm-text")
     public WebElement linkTextBox;
 
-    @FindBy(xpath = "//input[@id='video_idPostFormLHE_blogPostForm-source']")
+    @FindBy(xpath = "//input[@placeholder='Link URL']")
     public WebElement linkUrlBox;
+
+    @FindBy(xpath = "//input[@placeholder='YouTube or Vimeo video URL']")
+    public WebElement linkUrlBoxForVideo;
 
     @FindBy(xpath = "//img[@class='bxhtmled-surrogate-dd']")
     public WebElement videoElementInBody;
