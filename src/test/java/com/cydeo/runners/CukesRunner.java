32dbc9544b58466bf5d002d3978ccd61367a89c4
zilaleshakir <zilaleshakir@gmail.com>
2022-11-24 10:54:48 +0300

    Merge remote-tracking branch 'origin/master'

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -17,8 +17,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = ""
-       // publish = true
+        tags = "@AZLC-1590",
+        publish = true
 )
 
 public class CukesRunner {
